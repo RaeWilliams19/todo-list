33ae6d8a718fc01a79abca4a81dd5f75528c6350
Rae Williams <syrae.williams@appliedtechnology.se>
2019-03-24 17:46:55 +0100

    modified style toggling and saved state in local storage

--- js/todo.js
+++ js/todo.js
@@ -1,128 +1,167 @@
-window.onload = function(){
-    addNewToDo();
-    getTasks();
+const taskArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+function generateTitle(title, element) {
+  const h1 = document.createElement('h1');
+  const text = document.createTextNode(title);
+  h1.setAttribute('class', 'card-title');
+  h1.appendChild(text);
+  element.appendChild(h1);
 }
 
-let taskArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
-
-function addNewToDo(){
-    document.getElementsByClassName('btn--add')[0].addEventListener('click', function() {    
-        const cardTitle = document.getElementById('title').value.toUpperCase();
-        const cardDescription = document.getElementById('description').value.toUpperCase();
-        
-        if (cardTitle === '' || cardDescription === ''){
-            alert('Oops! Please enter a title and description of your task');
-        } else {
-            const card = {
-                title: cardTitle,
-                description: cardDescription
-            }
-            taskArray.push(card);
-            createCard(cardTitle, cardDescription);
-        }
-        
-        localStorage.setItem('tasks', JSON.stringify(taskArray));
-        document.getElementById("title").value = '';
-        document.getElementById("description").value = '';
-    });
+function generateDescription(description, element) {
+  const h5 = document.createElement('h5');
+  const text = document.createTextNode(description);
+  h5.setAttribute('class', 'card-description');
+  h5.appendChild(text);
+  element.appendChild(h5);
 }
 
-function getTasks(){
-    if(localStorage.getItem('tasks')){
-        const data = JSON.parse(localStorage.getItem('tasks'));
-        data.forEach(obj => {
-            createCard(obj.title, obj.description);
-        })
+function deleteTask(element) {
+  const title = element.childNodes[0].textContent;
+  const description = element.childNodes[1].textContent;
+
+  for (let i = 0; i < taskArray.length; i += 1) {
+    const task = taskArray[i];
+    if (task.title === title && task.description === description) {
+      const taskIndex = taskArray.indexOf(task);
+      taskArray.splice(taskIndex, 1);
+      localStorage.setItem('tasks', JSON.stringify(taskArray));
     }
+  }
 }
 
-function createCard(title, description){
-    const div = document.createElement('div');
-    div.setAttribute('class', 'card');
+function createRemoveBtn(element, status) {
+  const button = document.createElement('button');
+  button.textContent = 'REMOVE';
 
-    const cardContainer = document.getElementsByClassName('card-container')[0];
-    cardContainer.appendChild(div);
-
-    generateTitle(title, div);
-    generateDescription(description, div);
-    createRemoveBtn(div)
-    generateTimeStamp(div);
-    taskStatus(div);
-}
-
-function generateTitle(title, element){
-    const h1 = document.createElement('h1');
-    const text = document.createTextNode(title)
-    h1.setAttribute('class', 'card-title');
-    h1.appendChild(text);
-    element.appendChild(h1);
-}
-
-function generateDescription(description, element){
-    const p = document.createElement('p');
-    const text = document.createTextNode(description);
-    p.setAttribute('class', 'card-description');
-    p.appendChild(text);
-    element.appendChild(p);
-}
-
-function taskStatus(element){
-    element.addEventListener('click', () => {
-        const el = element.children;
-        const removeBtn = element.children[2];
-        for (let i = 0; i<2; i++) {
-            const text = el[i];
-            if (text.style.textDecoration === 'none'){
-                text.style.textDecoration = 'line-through';
-                element.style.backgroundColor = '#DCF0D9';
-                removeBtn.style.display = 'inline';                
-            } else {
-                text.style.textDecoration = 'none';
-                element.style.backgroundColor = 'aliceblue';
-                removeBtn.style.display = 'none';                
-            }
-        }
-    });
-}
-
-function createRemoveBtn(element){
-    const button = document.createElement('button');
+  if (status) {
     button.setAttribute('class', 'btn btn--remove');
-    button.textContent = 'REMOVE';
-    button.style.display = 'none';
-    button.onclick =  function(){
-        deleteTask(element);
-        element.remove();
+  } else {
+    button.setAttribute('class', 'btn btn--remove hidden');
+  }
+
+  button.onclick = () => {
+    deleteTask(element);
+    element.remove();
+  };
+
+  element.appendChild(button);
+}
+
+function toggleTaskStatus(element) {
+  const title = element.childNodes[0].textContent;
+  const description = element.childNodes[1].textContent;
+  const removeBtn = element.childNodes[2];
+  const card = taskArray.find(task => task.title === title && task.description === description);
+
+  element.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'BUTTON') {
+      card.isDone = !card.isDone;
+      element.classList.toggle('done');
+      removeBtn.classList.toggle('hidden');
+      localStorage.setItem('tasks', JSON.stringify(taskArray));
     }
-    element.appendChild(button);
+  });
 }
 
-function deleteTask(element){
-    const taskTitle = element.childNodes[0].textContent;
-    const taskDescription = element.childNodes[1].textContent;
+function generateTimeStamp(element) {
+  const today = new Date().toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
-    for(let i = 0; i < taskArray.length; i++ ){
-        const obj = taskArray[i];
-        if(obj.title === taskTitle && obj.description === taskDescription){
-             const index = taskArray.indexOf(obj);
-             taskArray.splice(index, 1);
-             localStorage.setItem('tasks', JSON.stringify(taskArray));
-        } 
+  const p = document.createElement('p');
+  const text = document.createTextNode(today);
+  p.setAttribute('class', 'timestamp');
+  p.appendChild(text);
+  element.appendChild(p);
+}
+
+function createCard(title, description, status) {
+  const div = document.createElement('div');
+  const cardContainer = document.getElementsByClassName('card-container')[0];
+
+  if (status) {
+    div.setAttribute('class', 'card done');
+  } else {
+    div.setAttribute('class', 'card');
+  }
+
+  cardContainer.appendChild(div);
+  generateTitle(title, div);
+  generateDescription(description, div);
+  createRemoveBtn(div, status);
+  generateTimeStamp(div);
+  toggleTaskStatus(div);
+}
+
+function addNewToDo() {
+  const addTaskBtn = document.getElementsByClassName('btn--add')[0];
+  addTaskBtn.addEventListener('click', () => {
+    const cardTitle = document.getElementById('title').value.toUpperCase();
+    const cardDescription = document.getElementById('description').value.toUpperCase();
+
+    if (cardTitle === '' || cardDescription === '') {
+      alert('Oops! Please enter a title and description of your task');
+    } else {
+      const card = {
+        title: cardTitle,
+        description: cardDescription,
+        isDone: false,
+      };
+      taskArray.push(card);
+      createCard(cardTitle, cardDescription, card.isDone);
     }
+
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+  });
 }
 
-function generateTimeStamp(element){
-    const today = new Date().toLocaleDateString('en-GB', {  
-        day : 'numeric',
-        month : 'numeric',
-        year : 'numeric',
-        hour: '2-digit',
-	    minute: '2-digit'
-    })
-    
-    const p = document.createElement('p');
-    const text = document.createTextNode(today);
-    p.setAttribute('class', 'timestamp');
-    p.appendChild(text);
-    element.appendChild(p);
+function getTasks() {
+  if (localStorage.getItem('tasks')) {
+    const data = JSON.parse(localStorage.getItem('tasks'));
+    data.forEach((obj) => {
+      createCard(obj.title, obj.description, obj.isDone);
+    });
+  }
 }
+
+function sort() {
+  const sortAZ = document.getElementsByClassName('btn--sortAZ')[0];
+  const sortZA = document.getElementsByClassName('btn--sortZA')[0];
+
+  sortAZ.addEventListener('click', () => {
+    let taskItems = document.getElementsByClassName('card');
+    taskItems = [...taskItems];
+    taskItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+    for (let i = 0; i < taskItems.length; i += 1) {
+      const parent = taskItems[i].parentNode;
+      const detatchedTask = parent.removeChild(taskItems[i]);
+      parent.appendChild(detatchedTask);
+    }
+  });
+
+  sortZA.addEventListener('click', () => {
+    let taskItems = document.getElementsByClassName('card');
+    taskItems = [...taskItems];
+    taskItems.sort((a, b) => b.textContent.localeCompare(a.textContent));
+
+    for (let i = 0; i < taskItems.length; i += 1) {
+      const parent = taskItems[i].parentNode;
+      const detatchedTask = parent.removeChild(taskItems[i]);
+      parent.appendChild(detatchedTask);
+    }
+  });
+}
+
+window.onload = function start() {
+  addNewToDo();
+  getTasks();
+  sort();
+};
